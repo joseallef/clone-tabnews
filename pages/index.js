8bf959343a0e58533ea8b372d021ce3be1c0ea0c
Allef <46696111+joseallef@users.noreply.github.com>
2023-07-27 02:11:13 +0000

    add new dependency

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
+import React from 'react'
+import Confetti from 'react-confetti'
+
 function Home() {
-  return <h1>Teste 2 👍</h1>;
+  return (
+    <div className="">
+    <Confetti
+      width="1000vh"
+      height="1000vh !important"
+    >
+    <h1>Teste 1 👍</h1>
+    </Confetti>
+    </div>
+  );
 }
 
 export default Home;
