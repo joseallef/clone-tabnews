072d8c1a627a8efe9a1c05424d89932a4367a8dc
Allef <46696111+joseallef@users.noreply.github.com>
2023-11-05 17:13:35 +0000

    add `database.js` end `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
+import database from "../../../../infra/database.js";
 
-function status(request, response) {
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1 as sum;');
+  console.log("result", result.rows);
+
   response.status(200).json({ chave: "alunos do curso.dev são pessoas acima do média" });
 }
 
