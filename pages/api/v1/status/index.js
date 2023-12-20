7d0fb61d99259d7d787b3db503ed3869c0d25b5f
Allef <46696111+joseallef@users.noreply.github.com>
2023-12-20 18:45:14 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;');
-  console.log("result", result.rows);
+  const updatedAt = new Date().toISOString();
+  const dataVersionResult = await database.query("SHOW server_version;")
+  const databaseVersionValue = await dataVersionResult.rows[0].server_version;
 
-  response.status(200).json({ chave: "alunos do curso.dev são pessoas acima do média" });
+  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue = await databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
+  // "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';"
+  const databaseOpenedConnectionsValue = databaseOpenedConnectionResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue
+      }
+    }
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,4 +2,20 @@
 test("GET to /api/v1/status", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-})
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+});
+
+// Only test => indica ao jest que execute apneas esse test e pule o demais!
+// test.only("Teste de SQL Injection", async () => {
+//   await fetch("http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4); --");
+
+// })
