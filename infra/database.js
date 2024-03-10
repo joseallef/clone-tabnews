f7e03b00408eec028b0f2a04e6667b9bf7f27e69
Allef <46696111+joseallef@users.noreply.github.com>
2024-03-10 14:56:54 +0000

    adds migration scripts

--- infra/database.js
+++ infra/database.js
@@ -10,17 +10,6 @@ async function query(queryObject) {
     ssl: process.env.NODE_ENV === 'development' ? false : true,
   });
 
-
-  console.log("Credenciais do Postgres", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    environment: process.env.NODE_ENV
-  })
-
-
   try {
     await client.connect();
     const result = await client.query(queryObject);
@@ -32,7 +21,6 @@ async function query(queryObject) {
     await client.end();
   }
 }
-
 export default {
   query: query
 };
