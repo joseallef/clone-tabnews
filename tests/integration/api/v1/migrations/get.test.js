852dd80990fb3f7d3da6085ccf299bac7226865d
Allef <46696111+joseallef@users.noreply.github.com>
2024-09-14 19:52:28 +0000

    refactor: use `orchestrator.clearDatabase()` in tests

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,10 +1,9 @@
 /* eslint-disable jest/no-commented-out-tests */
-import database from "infra/database.js";
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await database.query("drop schema public cascade; create schema public");
+  await orchestrator.clearDatabase();
 });
 
 describe("GET /api/v1/migrations", () => {
