c4b61c3359bfa5120aad45ec9fe6533b4acbed73
Allef <46696111+joseallef@users.noreply.github.com>
2024-09-14 19:34:34 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -7,14 +7,18 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public");
 });
 
-test("GET to /api/v1/migrations", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
 
 // Only test => indica ao jest que execute apneas esse test e pule o demais!
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -7,29 +7,40 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public");
 });
 
-test("POST to /api/v1/migrations", async () => {
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response = await fetch("http://localhost:3000/api/v1/migrations", {
+          method: "POST",
+        });
+
+        expect(response.status).toBe(201);
+
+        const responseBody = await response.json();
+
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+
+      });
+
+      test("For the second time", async () => {
+        const response = await fetch("http://localhost:3000/api/v1/migrations", {
+          method: "POST",
+        });
+
+        expect(response.status).toBe(200);
+
+        const response2Body = await response.json();
+
+        expect(Array.isArray(response2Body)).toBe(true);
+        expect(response2Body.length).toBe(0);
+      });
+    });
   });
-
-  expect(response1.status).toBe(201);
-
-  const responseBody = await response1.json();
-
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
-
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-
-  expect(response2.status).toBe(200);
-
-  const response2Body = await response2.json();
-
-  expect(Array.isArray(response2Body)).toBe(true);
-  expect(response2Body.length).toBe(0);
 });
+
+
 
 // Only test => indica ao jest que execute apneas esse test e pule o demais!
 // test.only("Teste de SQL Injection", async () => {
